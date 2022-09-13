a0e3efd1f28d49f204530e7662ab443c60bb3f53
Lucas Weckermann <lw119377@fh-muenster.de>
2022-09-13 18:22:42 +0200

    clickContextMenuItem

--- scripts.js
+++ scripts.js
@@ -221,7 +221,8 @@ function getClickedElement(evt, className) {
 }
 
 function openSignContextMenu(evt, sign) {
-    var root = getConfigElementByUuid(config, sign.getAttributeNS(null, 'uuid'));
+    var uuid = sign.getAttributeNS(null, 'uuid');
+    var root = getConfigElementByUuid(config, uuid);
     var svg = getResource(`/signs/${root['sign']}.svg`);
     var re = /\{\{(\w+)/g;
     var newMenuItems = [];
@@ -232,10 +233,12 @@ function openSignContextMenu(evt, sign) {
             prefix = 'OK';
         var menuItem = document.createElement('li');
         menuItem.classList.add('context-menu-item');
+        menuItem.setAttribute('key', key);
         menuItem.appendChild(document.createTextNode(`${prefix}\t${key}`));
         newMenuItems.push(menuItem);
     }
     var menuItems = document.querySelector('.context-menu-items');
+    menuItems.setAttribute('uuid', uuid);
     menuItems.replaceChildren(...newMenuItems);
     
     var touchpos = getEvtPos(evt);
@@ -248,6 +251,20 @@ function openSignContextMenu(evt, sign) {
 function closeSignContextMenu() {
     var menu = document.querySelector('.context-menu');
     menu.classList.remove('context-menu-active');
+}
+
+function clickContextMenuItem(menuItem) {
+    var key = menuItem.getAttributeNS(null, 'key');
+    var uuid = menuItem.parentElement.getAttributeNS(null, 'uuid');
+    var root = getConfigElementByUuid(config, uuid);
+    if(root[key])
+    {
+        if(typeof root[key] === 'boolean')
+            root[key] = false;
+    }
+    else
+        root[key] = true;
+    draw();
 }
 
 function onDomContentLoaded() {
@@ -621,7 +638,7 @@ document.addEventListener('click', function (evt) {
     var menuItem = getClickedElement(evt, 'context-menu-item');
     if (menuItem) {
         evt.preventDefault();
-        console.log(menuItem);
+        clickContextMenuItem(menuItem);
     }
     closeSignContextMenu();
 });
