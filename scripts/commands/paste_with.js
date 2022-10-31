6cc3af2a8bfa98583f4505943572c9788b994d46
OdinOxin <OdinOxin@googlemail.com>
2022-10-31 15:09:16 +0100

    wip paste with

--- scripts/commands/paste_with.js
+++ scripts/commands/paste_with.js
@@ -4,10 +4,12 @@ PasteWithCmd.prototype.__proto__ = new AddCmd();
 
 PasteWithCmd.prototype.execute = function () {
     for (let i = 0; i < this.selectedElements.length; i++) {
-        var clone = this.cloneCachedElements();
-        delete clone.sub;
-        delete clone.with;
-        this.insertWith(this.selectedElements[i], clone);
+        var clonedElements = this.cloneCachedElements();
+        for (let idx in clonedElements) {
+            delete clonedElements[idx].sub;
+            delete clonedElements[idx].with;
+            this.insertWith(this.selectedElements[i], clonedElements[idx]);
+        }
     }
     return true;
 }
