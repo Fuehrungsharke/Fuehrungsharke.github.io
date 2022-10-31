cf7e08cb07e8e0fbd83f1a1cb2a76e190c3878f2
OdinOxin <OdinOxin@googlemail.com>
2022-10-31 15:18:46 +0100

    wip add cmds

--- scripts/commands/add_sib.js
+++ scripts/commands/add_sib.js
@@ -5,7 +5,7 @@ AddSibCmd.prototype.__proto__ = new AddCmd();
 AddSibCmd.prototype.execute = function () {
     for (let i = 0; i < this.selectedElements.length; i++) {
         var newSign = this.getNewSign(this.selectedElements[i], this.key);
-        this.insertSibling(newSign);
+        this.insertSibling(this.selectedElements[i], newSign);
     }
     return true;
 }
