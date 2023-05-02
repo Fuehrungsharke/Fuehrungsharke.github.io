414f7b53d3faac26984b3e9bd353e8afcd913883
OdinOxin <OdinOxin@googlemail.com>
2023-05-02 16:12:52 +0200

    wip test

--- tests/default.js
+++ tests/default.js
@@ -1,7 +1,14 @@
+const canvas = require('../scripts/canvas');
+
 module.exports = {
     BaseTest: function () {
         return 'BaseTest';
-    }
+    },
+
+    WipTest: function () {
+        canvas.draw();
+        return canvas.outputSvg;
+    },
 }
 
 require('make-runnable');
