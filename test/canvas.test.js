4461479cfbd4b46f2eaf5363fb0521b79c38b35b
OdinOxin <OdinOxin@googlemail.com>
2023-05-07 18:12:00 +0200

    canvas test

--- test/canvas.test.js
+++ test/canvas.test.js
@@ -1,20 +1,25 @@
 const canvas_mock = require('./mock/canvas.mock');
+const m_svg2png = require('convert-svg-to-png');
 
 describe("canvas tests", () => {
     test('draw single building sign', async () => {
         await canvas_mock.mock();
         const canvas = require('../scripts/canvas');
+        const m_resource_manager = require('../scripts/resource_manager');
+        
+        let svgExpected = m_resource_manager.getResourceAsync('/test/expected/Building.svg');
 
         await canvas.draw({
             'sign': 'Building',
-            'txt': 'OV',
             'colorPrimary': '#039',
             'colorAccent': '#FFF',
             'org': 'THW',
         });
 
         expect(
-            canvas.outputSvg.outerHTML
-        ).not.toBeNull();
+            await m_svg2png.convert(canvas.outputSvg.outerHTML)
+        ).toMatchObject(
+            await m_svg2png.convert(await svgExpected)
+        );
     });
 });
