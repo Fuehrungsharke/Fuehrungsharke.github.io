74ecaef968b5679f0304db89b8344218434f09da
OdinOxin <OdinOxin@googlemail.com>
2023-05-14 14:43:35 +0200

    multiply tests

--- test/canvas.test.js
+++ test/canvas.test.js
@@ -1,20 +1,22 @@
 const canvas_mock = require('./mock/canvas.mock');
 const m_svg2png = require('convert-svg-to-png');
+const fs = require('fs');
+const path = require('path');
+
+let configs = fs.readdirSync(path.join(__dirname, '../', '/test/configs/'));
 
 describe("canvas tests", () => {
-    test('draw single building sign', async () => {
+    test.each(configs
+        .map(c => [c, c.replace('.json', '.svg')])
+    )('draw %s', async (configFile, expectedFile) => {
         await canvas_mock.mock();
         const canvas = require('../scripts/canvas');
         const m_resource_manager = require('../scripts/resource_manager');
-        
-        let svgExpected = m_resource_manager.getResourceAsync('/test/expected/Building.svg');
 
-        await canvas.draw({
-            'sign': 'Building',
-            'colorPrimary': '#039',
-            'colorAccent': '#FFF',
-            'org': 'THW',
-        });
+        let svgExpected = m_resource_manager.getResourceAsync(`/test/expected/${expectedFile}`);
+        let jsonConfig = m_resource_manager.getResourceAsync(`/test/configs/${configFile}`);
+
+        await canvas.draw(JSON.parse(await jsonConfig));
 
         expect(
             await m_svg2png.convert(canvas.outputSvg.outerHTML)
